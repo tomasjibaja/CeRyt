ef5100f8b685b0139e92469b98cbb8ebb8d2c322
Tomas Jibaja <tomasjibaja@gmail.com>
2024-02-15 19:27:32 -0300

    mejorado el randomeo de celula

--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
 const notasList = [
     "negra",
-    "doblecorchea"
+    "doblecorchea",
+    "semis",
+    "semiCor",
+    "corSemi"
 ]
 
 const notas = {
     negra : "&#9833;",
-    doblecorchea : "&#9835;"
+    doblecorchea : "&#9835;",
+    semis : "&#9836;&#9836;",
+    semiCor : "&#9836;&#9834;",
+    corSemi : "&#9834;&#9836;"
 }
 
 function showTempo(valor) {
@@ -13,9 +19,10 @@ function showTempo(valor) {
 } 
 
 function generateCeryt() {
-    let beat1 = notasList[Math.round(Math.random())];
-    let beat2 = notasList[Math.round(Math.random())];
-    let beat3 = notasList[Math.round(Math.random())];
-    let beat4 = notasList[Math.round(Math.random())];
-    document.getElementById("frame").innerHTML = `<p class="ceryt">${notas[beat1]}${notas[beat2]}${notas[beat3]}${notas[beat4]}</p>`;
+    let beat1 = notasList[Math.round(Math.random() * 3)];
+    let beat2 = notasList[Math.round(Math.random() * 3)];
+    let beat3 = notasList[Math.round(Math.random() * 3)];
+    let beat4 = notasList[Math.round(Math.random() * 3)];
+    document.getElementById("frame").innerHTML = `<p class="ceryt">${notas[beat1]} ${notas[beat2]} ${notas[beat3]} ${notas[beat4]}</p>`;
 }
+
